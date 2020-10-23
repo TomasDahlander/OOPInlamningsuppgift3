0f1c1d440d6590bed3224bc01e1fbbcd0d88c1b0
Tomas Dahlander <tomas.dahlander@gmail.com>
2020-10-23 15:03:25 +0200

    Complete Tests

--- Test/FemtonSpelTest.java
+++ Test/FemtonSpelTest.java
@@ -10,13 +10,28 @@ public class FemtonSpelTest {
 
     @Test
     public final void nullFinderTest(){
-        go.labels[0] = new JButton("");
-        go.labels[1] = new JButton("1");
+        go.bricks[0] = new JButton(" ");
+        go.bricks[1] = new JButton("1");
 
         assertEquals(go.nullFinder(0),true);
         assertEquals(go.nullFinder(1),false);
     }
 
+    @Test
+    public final void convertArrayToStringTest(){
+        go.addLabels();
+        assertEquals(go.convertArrayToString(),"123456789101112131415 ");
+        assertNotEquals(go.convertArrayToString(),"123456789101112131514 ");
+    }
 
+    @Test
+    public final void isWinnerTest(){
+        go.addLabels();
+        System.out.println(go.convertArrayToString());
+        System.out.println("123456789101112131415 ");
+
+        assertTrue(go.isWinner());
+        assertNotEquals(go.isWinner(), "123456789101112131415");
+    }
 
 }
